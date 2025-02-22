68bef76f515619e059089b4f0d03351981a7a53e
ibrahim abdulquddus <ibrahimabdulquddus51@gmail.com>
2025-02-22 17:32:00 +0100

    refactor: update the interval time

--- src/config.js
+++ src/config.js
@@ -4,10 +4,12 @@ dotenv.config();
 console.log("DISCORD_TOKEN:", process.env.DISCORD_TOKEN ? "Token exists" : "Token is missing");
 console.log("TELEX_WEBHOOK:", process.env.TELEX_WEBHOOK);
 console.log("DISCORD_CHANNEL_ID:", process.env.DISCORD_CHANNEL_ID);
+console.log("PUBLIC_URL:", process.env.PUBLIC_URL);
 
 export const discordToken = process.env.DISCORD_TOKEN;
 export const telexWebhook = process.env.TELEX_WEBHOOK;
 export const discordChannelId = process.env.DISCORD_CHANNEL_ID;
 export const fetchInterval = parseInt(process.env.FETCH_INTERVAL) || 60000;
 export const maxMessages = parseInt(process.env.MAX_MESSAGES) || 10;
-export const port = process.env.PORT || 3000;
+export const port = process.env.PORT || 10000;
+export const publicUrl = process.env.PUBLIC_URL;
